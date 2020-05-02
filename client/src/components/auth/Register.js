857837866b8b6d35528d2f93d12dc93d2bd1e171
Mariappan <Mariappan.Kanakku@genesys.com>
2020-05-02 23:36:43 +0530

    React User Authntication

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,12 +1,13 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
 
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -20,8 +21,11 @@ const Register = ({ setAlert }) => {
         if (password !== password2) {
             setAlert("passwords does not match", "danger");
         } else {
-
+            register({ name, email, password });
         }
+    }
+    if (isAuthenticated) {
+        return <Redirect to="/dashboard" />
     }
     return (
         <Fragment>
@@ -29,7 +33,7 @@ const Register = ({ setAlert }) => {
             <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
             <form className="form" onSubmit={e => onSubmit(e)} >
                 <div className="form-group">
-                    <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+                    <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} />
                 </div>
                 <div className="form-group">
                     <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
@@ -67,6 +71,11 @@ const Register = ({ setAlert }) => {
     )
 }
 Register.propTypes = {
-    setAlert: PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
 }
-export default connect(null, { setAlert })(Register);
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+export default connect(mapStateToProps, { setAlert, register })(Register);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../../actions/auth';
+import PropTypes from 'prop-types';
+const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
 
-export const Navbar = () => {
+    const guestLinks = (
+        <ul>
+            <li><a href="#!">Developer</a></li>
+            <li><Link to="/register">Register</Link></li>
+            <li><Link to="/login">Login</Link></li>
+        </ul>
+    );
+    const authLinks = (<ul>
+        <li>
+            <a href="#!" onClick={logout}>
+                <i className="fas fa-sign-out-alt"></i>{' '}
+                <span className="hide-sm">Logout</span>
+            </a>
+        </li>
+    </ul>);
     return (
         <nav className="navbar bg-dark">
             <h1>
                 <Link to="/index"><i className="fas fa-code"></i> DevConnector</Link>
             </h1>
-            <ul>
-                <li><a href="!#">Developers</a></li>
-                <li><Link to="/register">Register</Link></li>
-                <li><Link to="/login">Login</Link></li>
-            </ul>
+            {!loading && (<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>)}
         </nav>
     )
 }
+Navbar.propTypes = {
+    logout: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
+const mapStateToProps = state => ({
+    auth: state.auth
+
+})
+export default connect(mapStateToProps, { logout })(Navbar);
