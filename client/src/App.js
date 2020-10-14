8abed029d25c7cfa9ba8199ea23b275a8e9880c2
Mariappan <Mariappan.Kanakku@genesys.com>
2020-10-14 13:45:49 +0530

    removed secret

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import { Landing } from './components/layout/Landing';
+import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import PrivateRoute from './components/routing/PrivateRoute';
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './actions/auth'
 import './App.css';
@@ -30,6 +33,9 @@ const App = () => {
             <Switch>
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+
             </Switch>
           </section>
         </Fragment>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -14,6 +14,12 @@ const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
     );
     const authLinks = (<ul>
         <li>
+            <Link to="/dashboard">
+                <i className="fas fa-user"></i>{' '}
+                <span className="hide-sm">Dashboard</span>
+            </Link>
+        </li>
+        <li>
             <a href="#!" onClick={logout}>
                 <i className="fas fa-sign-out-alt"></i>{' '}
                 <span className="hide-sm">Logout</span>
@@ -23,7 +29,7 @@ const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
     return (
         <nav className="navbar bg-dark">
             <h1>
-                <Link to="/index"><i className="fas fa-code"></i> DevConnector</Link>
+                <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
             </h1>
             {!loading && (<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>)}
         </nav>
